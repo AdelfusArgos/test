bac243147cfe6a31814e2eca40837b286a3cbd3c
Vladimir <adelfusargoss@gmail.com>
2021-04-01 17:28:14 +0600

    test3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-let age = "b";
+let money = prompt('Ваш бюджет на месяц?',''),
+time = prompt('Введите дату в формате YYYY-MM-DD');
+
+
+let appData = {
+    budget:money,
+    timeData:time,
+
+ 
+
+    optionalExpsens:{},
+    income:[''],
+    savings:false,
+
+}
+
+
+
+
+alert((money / 24) )
